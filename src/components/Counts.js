a794477b1290e5ad64adde7df4c8db57ed794cfc
Graydon Wasil <arrangedgodly@gmail.com>
2022-12-21 18:18:53 -0700

    secondary refactor: setting up the pages schematic for accessing previous counts

--- src/components/Counts.js
+++ src/components/Counts.js
@@ -1,7 +1,37 @@
+import React from 'react';
+
 function Counts() {
+  const [store, setStore] = React.useState(null);
+
   return (
     <main>
-
+      {store === null ? (
+      <form>
+        <p>Pick your Store:</p>
+        <label
+          htmlFor='quincy'
+        >
+          <input
+            type='radio'
+            value='quincy'
+          />
+          Quincy
+        </label>
+        <label
+          htmlFor='sheridan'
+        >
+          <input
+            type='radio'
+            value='sheridan'
+          />
+          Sheridan
+        </label>
+      </form>
+      ) : (
+      <div>
+        <p>Your Store is:</p>
+      </div>
+      )} 
     </main>
   );
 }
