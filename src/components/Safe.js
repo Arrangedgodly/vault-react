335de81175e72a0be5482d8a6980c73709af480d
Arrangedgodly <90996993+Arrangedgodly@users.noreply.github.com>
2022-12-22 17:44:08 -0700

    refactor: fixing the store navigation page

--- src/components/Safe.js
+++ src/components/Safe.js
@@ -1,56 +1,78 @@
-import React from 'react';
-import Buttons from './Buttons';
-import ButtonsAlt from './ButtonsAlt';
-import { buttonsData } from '../utils/constants';
-import { CurrentSafeTotalContext } from '../contexts/CurrentSafeTotalContext';
+import React from "react";
+import Buttons from "./Buttons";
+import ButtonsAlt from "./ButtonsAlt";
+import { buttonsData } from "../utils/constants";
+import { CurrentSafeTotalContext } from "../contexts/CurrentSafeTotalContext";
 
 function Safe() {
-    const safe = React.useContext(CurrentSafeTotalContext);
-    const totalArray = Object.keys(safe);
-    const [safeTotal, setSafeTotal] = React.useState(0);
+  const safe = React.useContext(CurrentSafeTotalContext);
+  const totalArray = Object.keys(safe);
+  const [safeTotal, setSafeTotal] = React.useState(0);
 
-    const handleSafeTotal = () => {
-      let total = 0;
-      for (let i = 0; i < totalArray.length; i++) {
-        total += safe[totalArray[i]];
-      }
-      setSafeTotal(total);
+  const handleSafeTotal = () => {
+    let total = 0;
+    for (let i = 0; i < totalArray.length; i++) {
+      total += safe[totalArray[i]];
     }
+    setSafeTotal(total);
+  };
 
-    return (
-        <form id='safe'>
-            {
-                buttonsData.map(buttons => (
-                    <Buttons 
-                        solo={buttons.solo}
-                        group={buttons.group}
-                        single={buttons.single}
-                        bunch={buttons.bunch}
-                        container={buttons.container}
-                        button={buttons.button}
-                        count={buttons.count}
-                        square={buttons.square}
-                        name={buttons.name}
-                        key={buttons.name}
-                        handleSafeTotal={handleSafeTotal}
-                    />
-                ))
-            }
-            <ButtonsAlt
-              handleSafeTotal={handleSafeTotal}
-            />
-            <div className="containers_alt">
-              <h2 className="button__label">Total</h2>
-              <div className="count" id="totalCount">
-                {safeTotal}
-              </div>
-              <div className={safeTotal === 6500 ? "confirmation confirmation__yes" : "confirmation"}>
-              <p className="confirmation__text">PERIOD</p>
-              <p className="confirmation__text">POOH!</p>
-              </div>
-            </div>
-        </form>
-    );
+  const handleReset = () => {
+    localStorage.clear();
+    window.location.reload();
+  }
+
+  return (
+    <form id="safe">
+      {buttonsData.map((buttons) => (
+        <Buttons
+          solo={buttons.solo}
+          group={buttons.group}
+          single={buttons.single}
+          bunch={buttons.bunch}
+          container={buttons.container}
+          button={buttons.button}
+          count={buttons.count}
+          square={buttons.square}
+          name={buttons.name}
+          key={buttons.name}
+          handleSafeTotal={handleSafeTotal}
+        />
+      ))}
+      <ButtonsAlt handleSafeTotal={handleSafeTotal} />
+      <div className="containers_alt">
+        <h2 className="button__label">Total</h2>
+        <div className="count" id="totalCount">
+          {safeTotal}
+        </div>
+        <div
+          className={
+            safeTotal === 6500
+              ? "confirmation confirmation__yes"
+              : "confirmation"
+          }
+        >
+          <p className="confirmation__text">PERIOD</p>
+          <p className="confirmation__text">POOH!</p>
+        </div>
+        <div className='footer__buttons'>
+          <button
+            type="button"
+            className="footer__button"
+          >
+            Submit
+          </button>
+          <button
+            type="button"
+            className="footer__button"
+            onClick={handleReset}
+          >
+            Reset
+          </button>
+        </div>
+      </div>
+    </form>
+  );
 }
 
 export default Safe;
